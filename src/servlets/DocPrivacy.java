4aab888c6fea7b827bbfea451e988fc07994586b
moras18 <claudio.more94@gmail.com>
2018-11-15 11:08:48 +0100

    visualizzazione documento privacy
    ok

---
+++ src/servlets/DocPrivacy.java
@@ -0,0 +1,46 @@
+package servlets;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import util.FreeMarker;
+
+/**
+ * Servlet implementation class DocPrivacy
+ */
+@WebServlet("/DocPrivacy")
+public class DocPrivacy extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	Map data= new HashMap<String, Object>();  
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DocPrivacy() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		FreeMarker.process("docprivacy.html", data, response, getServletContext());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		FreeMarker.process("docprivacy.html", data, response, getServletContext());
+	}
+
+}
